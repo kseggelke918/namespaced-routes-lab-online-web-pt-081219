6219a6e20315d72010aa90ff5243a812408e715b
Katie Seggelke <katie.seggelke@gmail.com>
2020-01-30 20:32:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200130202447) do
+ActiveRecord::Schema.define(version: 20200130203234) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20200130202447) do
   end
 
   create_table "preferences", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.boolean  "allow_create_artists"
+    t.boolean  "allow_create_songs"
   end
 
   create_table "songs", force: :cascade do |t|
